944c3716dfda288aa3f91dca39ca581bd587e6b3
Alexander von Trostorff <alexander.vontrostorff@rewe-group.com>
2026-02-05 17:07:37 +0100

    Mobile menu

--- src/app/nav/nav.ts
+++ src/app/nav/nav.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
+  standalone: true,
   imports: [RouterLink],
   templateUrl: './nav.html',
   styleUrl: './nav.css',
 })
 export class Nav {
+  readonly isMenuOpen = signal(false);
+
+  toggleMenu() {
+    this.isMenuOpen.update(v => !v);
+  }
+
+  closeMenu() {
+    this.isMenuOpen.set(false);
+  }
 }
